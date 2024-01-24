cbeff35c91288402c4f69de548ffdda1a3f75cbe
Reysmer Valle <reysmerwvr@gmail.com>
2024-01-24 21:40:48 +0100

    improve state test case

--- src/__tests__/state.js
+++ src/__tests__/state.js
@@ -4,8 +4,14 @@ import userEvent from '@testing-library/user-event'
 import {FavoriteNumber} from '../favorite-number'
 
 test('entering an invalid value shows an error message', () => {
-  render(<FavoriteNumber />)
+  const {rerender} = render(<FavoriteNumber />)
   const input = screen.getByLabelText(/favorite number/i)
+
   userEvent.type(input, '10')
+
   expect(screen.getByRole('alert')).toHaveTextContent(/the number is invalid/i)
+
+  rerender(<FavoriteNumber max={10} />)
+
+  expect(screen.queryByRole('alert')).not.toBeInTheDocument()
 })
